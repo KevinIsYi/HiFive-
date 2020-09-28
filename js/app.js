bb9421d39910981cb03c5a6ad5988f01f5a4108e
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-09-28 18:09:51 -0500

    Featured section is now responsive

--- js/app.js
+++ js/app.js
@@ -6,13 +6,19 @@ const headerImages = [
 ];
 
 const arrowsHeader = document.querySelectorAll('.arrows i');
-let currImgIndex = 0;
+let currImgIndex = 0, featuredImgIndex = 0;
 
 const eventListeners = () => {
 
-    document.addEventListener('DOMContentLoaded', setBackImage(0) );
+    document.addEventListener('DOMContentLoaded', () => {
+        setBackImage(0);
+        placeFeaturedImages();
+    });
 
-    window.addEventListener('resize', () => setBackImage(currImgIndex) );
+    window.addEventListener('resize', () => {
+        setBackImage(currImgIndex);
+        placeFeaturedImages();
+    });
     
     arrowsHeader[0].addEventListener('click', () => {
         if (--currImgIndex < 0) {
@@ -27,6 +33,50 @@ const setBackImage = (switchTo) => {
     const isScreenBig = screen.width >= 768 ? 0 : 1;
     const url = `url(../images/${ headerImages[switchTo % 4][isScreenBig] })`;
     document.querySelector('.back-img').style.backgroundImage = url;
+}
+
+const placeFeaturedImages = () => {
+    const screenSize = screen.width;
+    const featured = document.querySelector('.featured');
+
+    let numberOfImages = 1;
+
+    cleanFeaturedArea(featured);
+
+    if (screenSize >= 900) {
+        numberOfImages = 4;
+    } else if (screenSize >= 768) {
+        numberOfImages = 3;
+    } else if (screenSize >= 480) {
+        numberOfImages = 2;
+    }
+
+    for (let i = 1 ; i <= numberOfImages ; ++i) {
+        const newFeaturedProduct = document.createElement('div');
+
+        newFeaturedProduct.className = 'featured-products';
+        newFeaturedProduct.innerHTML = `
+            <img src="./images/featured-0${i}.jpg" alt="featured-0${i}">
+            <p class="in-image">New</p>
+            <i class="fas fa-heart"></i>
+            <button class="btn btn-add-cart">Add To Cart</button>
+            <div class="specifications">
+                <p>Black Dress - Denim</p>
+                <div class="price-offer">
+                    <p>$75.00</p>
+                    <p>$49.99</p>
+                </div>
+            </div>
+        `;
+        featured.appendChild(newFeaturedProduct);
+    }
+}
+
+const cleanFeaturedArea = (featured = document.querySelector('.featured')) => {
+    while(featured.firstChild) {
+        featured.removeChild(featured.firstChild);
+    }
+    console.log("Se llama");
 }
 
 eventListeners();
