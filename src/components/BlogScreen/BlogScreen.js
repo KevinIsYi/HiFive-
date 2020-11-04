fea48760d44871473e243a9ce449bf516a9e1c8e
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-11-04 14:10:57 -0600

    finished with blog page

--- src/components/BlogScreen/BlogScreen.js
+++ src/components/BlogScreen/BlogScreen.js
@@ -3,12 +3,11 @@ import { BannerImage } from '../BannerImage/BannerImage';
 
 import { blogData } from '../../data/blogData';
 import { BlogPost } from './BlogPost/BlogPost';
+import { recentPosts } from '../../data/recentPosts';
 
 import './BlogScreen.css';
 
 export const BlogScreen = () => {
-
-
 
     return (
         <>
@@ -18,7 +17,7 @@ export const BlogScreen = () => {
                 height={ 50 }
             />
             <section className="blog-section">
-                
+                <div>
                     { 
                         blogData.map((post, index) => (
                             <BlogPost 
@@ -26,8 +25,34 @@ export const BlogScreen = () => {
                                 data={ post } />
                         ))
                     }
+                </div>
                 <div className="blog-filters">
-
+                    <div className="search-blog">
+                        <input type="text" placeholder="Search Keyword"/>
+                        <button className="btn">Search</button>
+                    </div>
+                    <div className="blog-filter-section">
+                        <h1>Category</h1>
+                        <p>Restaurant Food</p>
+                        <p>Travel</p>
+                        <p>Trending</p>
+                        <p>Inspiration</p>
+                        <p>Healt Care</p>
+                    </div>
+                    <div className="blog-filter-section">
+                        <h1>Recent Posts</h1>
+                        {
+                            recentPosts.map(({ img, tittle, date }, index) => (
+                                <div className="recent-post-blog" key={ index }>
+                                    <img src={ `./assets/blog/${ img }.jpg` } alt={ img } />
+                                    <div className="recent-post-info">
+                                        <h2>{ tittle }</h2>
+                                        <p>{ date }</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </section>
         </>
---
+++ src/data/recentPosts.js
@@ -0,0 +1,23 @@
+export const recentPosts = [
+    {
+        img: 'recent-01',
+        tittle: 'Winter Is Coming',
+        date: 'October 12, 2020'
+    },
+    {   
+        img: 'recent-02',
+        tittle: 'Exploring Time',
+        date: 'January 30, 2020'
+
+    },
+    {
+        img: 'recent-03',
+        tittle: 'New year, new me',
+        date: 'December 31, 2019'
+    },
+    {
+        img: 'recent-04',
+        tittle: 'Be the best chef',
+        date: 'November 17, 2019'
+    }
+];
