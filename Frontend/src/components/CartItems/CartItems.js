4dd165345f0b55bfbbb595bd7e29821d10fd9372
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-11-26 11:30:43 -0600

    Changed css and add functions to read and write on localstorage

--- Frontend/src/components/CartItems/CartItems.js
+++ Frontend/src/components/CartItems/CartItems.js
@@ -1,45 +1,65 @@
-import React, { useState } from 'react';
-import { RiDeleteBack2Line } from 'react-icons/ri';
+import React, { useContext, useEffect, useState } from 'react';
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../hooks/useUserContext';
 
 import './CartItems.css';
 
 export const CartItems = () => {
 
-    const [ elements, setElements ] = useState(0);
+    const [ cartItems, setCartItems ] = useState([]);
+    const { isLogged } = useContext(UserContext);
+    const [ total, setTotal ] = useState(0);
+
+    const getCartItems = async () => {
+        const items = JSON.parse(localStorage.getItem('scitems'));
+        let total = 0;
+        items.forEach(({ price }) => {
+            total += price;
+        });
+        setCartItems(items);
+        setTotal(total);
+    }
+    useEffect( () => {
+        getCartItems();
+    }, [])
+
+    const clickedNumberItems = (id, quantity, value, index) => {
+        /*
+        const newQuantity = Math.max(0, quantity + value);
+        const { price } = cartItems[index];
+        const totalBefore = cartItems[index].quantity * price;
+        setCartItems(cartItems.map(item => item._id === id ? { ...item, quantity: newQuantity } : item));
+        const newTotal = newQuantity * price;
+
+        setTotal(total + (newTotal - totalBefore));
+        */
+    }
 
     return (
-        <div className="cart-container">
-            <h1>{ elements } Items in your shopping bag </h1>
-            <p>Items</p>
-            <div className="items">
-                <div className="cart-item">
-                    <img src="./assets/items/AB01.jpg" alt="AB01" />
-                    <div className="cart-item-description">
-                        <p className="item-description">Louis Vuitton Tote Bag Onthego (White) Handbag </p>
-                        <div className="price-breakdown">
-                            <p># Items: <span>3</span></p>
-                            <p>Unit Price: <span>$360</span></p>
-                            <p>Total: <span>$1080</span></p>
-                        </div>
-                        <RiDeleteBack2Line className="icon delete-icon" />
-                    </div>
+        <div className="shopping-cart-section">
+            <div className="total-section">
+                <div className="div-total-description">
+                    <p>Total: </p>
+                    <h1>${ total.toFixed(2) }</h1>
                 </div>
-                
-                <div className="cart-item">
-                    <img src="./assets/items/AB01.jpg" alt="AB01" />
-                    <div className="cart-item-description">
-                        <p className="item-description">Louis Vuitton Tote Bag Onthego (White) Handbag </p>
-                        <div className="price-breakdown">
-                            <p># Items: <span>3</span></p>
-                            <p>Unit Price: <span>$360</span></p>
-                            <p>Total: <span>$1080</span></p>
-                        </div>
-                        <RiDeleteBack2Line className="icon delete-icon" />
-                    </div>  
-                </div>
+                <Link to="/checkout">
+                    <button className="btn btn-total">Pay</button>
+                </Link>
             </div>
-            <Link to="checkout"><button className="btn btn-checkout">Proceed to Checkout</button></Link>
+            <div className="shopping-cart-items">
+                { 
+                    cartItems.map(({ _id, img, name, price }) => (
+                        <div className="shopping-cart-item" key={ _id }>
+                            <img src={`./assets/items/${ img }.jpg`} alt={ img } />
+                            <div className="description-price">
+                                <h1>{ name } </h1>
+                                <p><span>Price:</span> ${ price }</p>
+                            </div>
+                        </div>
+                    )) 
+                }
+            </div>
         </div>
     )
 }
