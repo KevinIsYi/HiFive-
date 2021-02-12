b4d43a1a902280b1c5192b272f07eb457d27cc49
Kevin <rodriguezkevin95g@gmail.com>
2021-02-12 17:48:31 -0600

    Added images

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -6,26 +6,108 @@ const router = Router();
 
 router.get('/', (req, res) => {
     return res.json({
-        ok: true
+        data: [
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB01`,
+                'category': 'bags',
+                'name': 'Bag 01',
+                'price': 1265.12,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB02`,
+                'category': 'bags',
+                'name': 'Bag 02',
+                'price': 2234.09,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB03`,
+                'category': 'bags',
+                'name': 'Bag 03',
+                'price': 1459.32,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB04`,
+                'category': 'bags',
+                'name': 'Bag 04',
+                'price': 2113.32,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB05`,
+                'category': 'bags',
+                'name': 'Bag 05',
+                'price': 1357,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB06`,
+                'category': 'bags',
+                'name': 'Bag 06',
+                'price': 2459.99,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB07`,
+                'category': 'bags',
+                'name': 'Bag 07',
+                'price': 1375.65,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB08`,
+                'category': 'bags',
+                'name': 'Bag 08',
+                'price': 987.14,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB09`,
+                'category': 'bags',
+                'name': 'Bag 09',
+                'price': 2149.12,
+                'available': 20
+            },
+            {
+                'image': `https://hi-five-backend-server.herokuapp.com/public/images/AB10`,
+                'category': 'bags',
+                'name': 'Bag 10',
+                'price': 965.34,
+                'available': 20
+            },
+        ]   
     })
 });
 
 router.post( '/insert-items', uploadOptions.single('image'), async (req, res) => {
         
     try {
-        const { body, protocol, file:{ filename } } = req;
+        const { body, file} = req;
         
-        const imagePath = `${protocol}://${req.get('host')}/public/images/${filename}`;
+        if (!file) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Image is required'
+            });
+        }
+        
+        const { filename } = file; 
+        const imagePath = `https://hi-five-backend-server.herokuapp.com/public/images/${filename}`;
 
         const newProduct = new Product(body);
         newProduct.image = imagePath;
 
-        
+        await newProduct.save();
 
         res.json({
-            ok: true
+            ok: true,
+            product: newProduct
         });
+
     } catch (error) {
+        console.log(error);
         res.json({
             ok: false
         });
